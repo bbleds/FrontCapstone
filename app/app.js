c4d46bac82b335a8a91eca2d8e1cc56bc5ddb586
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-05 15:21:06 -0600

    added begining functionality for login, and initialized firebase link in html, along with  as dependency inside loginCtrl.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
 //initialzes app and sets up routes
+var app = angular.module("MainApp", 
+
+	//deps
+	["firebase","ngRoute"]);
+
+
+//routes
+app.config(["$routeProvider", function($routeProvider){
+	$routeProvider
+
+	//login route
+	.when("/login", {
+		templateUrl: "app/partials/login.html",
+		controller: "loginCtrl"
+	})
+
+	//default to login route
+	.otherwise("/login");
+}])
