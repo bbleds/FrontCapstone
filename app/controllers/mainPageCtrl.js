381e04c80c796e06d68a67a8fe0f426ef427e10b
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-10 15:00:26 -0600

    added basic functionlaity for searching games and outputting

--- app/controllers/mainPageCtrl.js
+++ app/controllers/mainPageCtrl.js
@@ -8,4 +8,74 @@ function($firebaseArray, $scope, $location, $rootScope, generalVariables){
 	var ref = new Firebase("https://frontcapstone.firebaseio.com");
 	generalVariables.checkUserLogin("mainPage");
 
+	//logOut user
+	$scope.logOutUser = function(){
+
+		//unauth ref
+		ref.unauth();
+
+		//navigate back to main
+		$location.path("/login");
+	}
+
+
+	//game variables
+	$scope.gameTitle;
+	$scope.gameMaxPlayers;
+	$scope.gameMinPlayers;
+	$scope.gameTime;
+	$scope.gameAddress;
+	$scope.gameState;
+	$scope.gameCity;
+	$scope.gameDate;
+
+	//creation success variable
+	$scope.gameCreationSuccess;
+
+	//create Game
+	$scope.createGame = function(){
+		
+		//create Game in reference 
+		console.log("appleSauce");
+
+		if( $scope.gameTitle && $scope.gameMaxPlayers && $scope.gameMinPlayers && $scope.gameTime && $scope.gameAddress && $scope.gameState && $scope.gameCity){
+
+			ref.child("Games").push({
+				"sportTitle": $scope.gameTitle,
+				"maxPlayers": $scope.gameMaxPlayers,
+				"minPlayers": $scope.gameMinPlayers,
+				"time" : $scope.gameTime,
+				"address": $scope.gameAddress,
+				"city" : $scope.gameCity,
+				"state": $scope.gameState,
+				"date": $scope.gameDate
+			});
+
+			//display success
+			$scope.gameCreationSuccess = "Game Creation Successful!";
+
+		} else {
+			console.log("you need to enter all fields");
+		}
+
+	}
+
+
+	//find variables
+	$scope.findCity;
+	$scope.findState;
+	$scope.gamesFound;
+
+	//Find Game
+	$scope.findGames = function(){
+		var games = $firebaseArray(ref.child("Games"));
+
+		games.$loaded()
+		.then(function(data){
+			console.log("data ", data);
+			$scope.gamesFound = data;
+
+		});
+	}
+
 }]);
