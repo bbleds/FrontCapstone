aa8c35c11720f8026873eef2bdd375cba56ab58a
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-12 11:51:31 -0600

    Added time and date picker

--- app/controllers/mainPageCtrl.js
+++ app/controllers/mainPageCtrl.js
@@ -81,6 +81,112 @@ function($firebaseArray, $scope, $location, $rootScope, generalVariables){
 
 	}
 
+	//time picker functions from angular bootstrap ui
+			  $scope.mytime = new Date();
+
+			  $scope.hstep = 1;
+			  $scope.mstep = 1;
+
+			  $scope.options = {
+			    hstep: [1, 2, 3],
+			    mstep: [1, 5, 10, 15, 25, 30]
+			  };
+
+			  $scope.ismeridian = true;
+			  $scope.toggleMode = function() {
+			    $scope.ismeridian = ! $scope.ismeridian;
+			  };
+
+			  $scope.update = function() {
+			    var d = new Date();
+			    d.setHours( 14 );
+			    d.setMinutes( 0 );
+			    $scope.mytime = d;
+			  };
+
+			  $scope.changed = function () {
+			    console.log('Time changed to: ' + $scope.gameTime);
+			  };
+
+			  $scope.clear = function() {
+			    $scope.mytime = null;
+			  };
+
+
+	 //datepicker functions from angular bootstrap ui
+			  $scope.today = function() {
+			    $scope.gameDate = new Date();
+			  };
+			  $scope.today();
+
+			  $scope.clear = function () {
+			    $scope.gameDate = null;
+			  };
+
+			  // Disable weekend selection
+			  $scope.disabled = function(date, mode) {
+			    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+			  };
+
+			  $scope.toggleMin = function() {
+			    $scope.minDate = $scope.minDate ? null : new Date();
+			  };
+			  $scope.toggleMin();
+			  $scope.maxDate = new Date(2020, 5, 22);
+
+			  $scope.open = function($event) {
+			    $scope.status.opened = true;
+			  };
+
+			  $scope.setDate = function(year, month, day) {
+			    $scope.gameDate = new Date(year, month, day);
+			  };
+
+			  $scope.dateOptions = {
+			    formatYear: 'yy',
+			    startingDay: 1
+			  };
+
+			  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+			  $scope.format = $scope.formats[0];
+
+			  $scope.status = {
+			    opened: false
+			  };
+
+			  var tomorrow = new Date();
+			  tomorrow.setDate(tomorrow.getDate() + 1);
+			  var afterTomorrow = new Date();
+			  afterTomorrow.setDate(tomorrow.getDate() + 2);
+			  $scope.events =
+			    [
+			      {
+			        date: tomorrow,
+			        status: 'full'
+			      },
+			      {
+			        date: afterTomorrow,
+			        status: 'partially'
+			      }
+			    ];
+
+			  $scope.getDayClass = function(date, mode) {
+			    if (mode === 'day') {
+			      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+			      for (var i=0;i<$scope.events.length;i++){
+			        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+			        if (dayToCheck === currentDay) {
+			          return $scope.events[i].status;
+			        }
+			      }
+			    }
+
+			    return '';
+			  };
+
+
 
 	//find variables
 	$scope.findCity;
