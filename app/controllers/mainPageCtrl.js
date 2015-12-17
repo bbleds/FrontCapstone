3b8160aafc90af6dfa1e8e52ff6f175beb104be6
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-17 11:08:48 -0600

    enabled weekend selections for date

--- app/controllers/mainPageCtrl.js
+++ app/controllers/mainPageCtrl.js
@@ -193,9 +193,9 @@ function($firebaseArray, $scope, $location, $rootScope, $http, generalVariables)
 			  };
 
 			  // Disable weekend selection
-			  $scope.disabled = function(date, mode) {
-			    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-			  };
+			  // $scope.disabled = function(date, mode) {
+			  //   return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+			  // };
 
 			  $scope.toggleMin = function() {
 			    $scope.minDate = $scope.minDate ? null : new Date();
