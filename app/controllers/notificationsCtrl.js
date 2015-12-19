32a3ceaba7b507ff4dd3f625003006d89b0bc114
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-19 12:57:17 -0600

    added comments for clarity in the matter of the log in and log out parsing bug in mainCtrl and notificationsCtrl

--- app/controllers/notificationsCtrl.js
+++ app/controllers/notificationsCtrl.js
@@ -6,6 +6,7 @@ function($firebaseArray, $scope, $location, $rootScope, $http, generalVariables)
 	//reference to database
 	var ref = new Firebase("https://frontcapstone.firebaseio.com");
 
+	//instead of running onauth in general variables, we run it here because the code was being parsed in an unexpected sequence which cause generalVairables.getUid to be undefined in the code below. Since we are checking directly in this file for auth data, (and in mainPageCtrl), it elminates those errors and unexpected behavior.
 	ref.onAuth(function(authdata){
 		console.log("authdata ", authdata);
 		if(authdata === null){
