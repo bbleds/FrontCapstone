f6b22401ab0c530f96a7cf5e6b02db57379173a2
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-28 15:31:59 -0600

    added padding to sidebar and fixed expand bug on game icon in sidebar

--- app/app.js
+++ app/app.js
@@ -46,3 +46,16 @@ app.config(["$routeProvider", function($routeProvider){
 	//default to login route
 	.otherwise("/login");
 }])
+
+//hide and show game options
+$("#gameSelector").click(function(){
+
+	if($("#gameSpacer").height()< 5){
+		$("#gameSpacer").animate({"height":"204px"}, 200);
+	} else if ($("#gameSpacer").height() >5){
+		$("#gameSpacer").animate({"height":"0px"}, 300);
+	}
+});
+
+
+
