c469a9d6360b41ba83c327e6f21a4de9a1decc5f
Ben Bledsoe <bledsoedesigns@gmail.com>
2015-12-30 01:10:17 -0600

    completed styles on currentGames page

--- app/app.js
+++ app/app.js
@@ -26,10 +26,10 @@ app.config(["$routeProvider", function($routeProvider){
 		templateUrl: "app/partials/currentGames.html",
 		controller: "currentGamesCtrl"
 	})
-	.when("/finishedGames",  {
-		templateUrl: "app/partials/finishedGames.html",
-		controller: "finishedGamesCtrl"
-	})
+	// .when("/finishedGames",  {
+	// 	templateUrl: "app/partials/finishedGames.html",
+	// 	controller: "finishedGamesCtrl"
+	// })
 	.when("/profile",  {
 		templateUrl: "app/partials/userProfile.html",
 		controller: "userProfileCtrl"
